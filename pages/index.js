92731995ab9fa4c778d46733c95bb802cecee2e7
amine alaoui <omari1amineco@gmail.com>
2024-02-07 22:13:27 +0100

    feat: add the body style to home page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import MainContainer from "@/components/MainContainer";
 
 export default function Home() {
   return (
-    <main className="mx-2 flex h-screen items-center justify-center duration-500">
+    <main className="body">
       <MainContainer />
     </main>
   );
