7b0f5bc2588ca890fa1cd122d5b63b1308a8a696
amine alaoui <omari1amineco@gmail.com>
2024-02-07 21:57:09 +0100

    refactor: MainContainer component

--- components/MainContainer.jsx
+++ components/MainContainer.jsx
@@ -15,29 +15,24 @@ const MainContainer = () => {
   };
 
   return (
-    <div className="w-full max-w-[550px] rounded-2xl border-4 border-zinc-700 p-6 dark:border-white">
-      <div className="mb-8 flex justify-center">
-        <button
-          onClick={getQuote}
-          className="rounded-full border bg-zinc-700 px-6 py-2 text-sm text-white duration-300 hover:border-zinc-700 hover:bg-white hover:text-zinc-700 hover:shadow-2xl md:px-8 md:py-3 md:text-lg dark:bg-white dark:text-zinc-700 dark:hover:border-white dark:hover:bg-zinc-700 dark:hover:text-white"
-        >
+    <div className="">
+      <div className="">
+        <button onClick={getQuote} className="">
           Get a New Quote
         </button>
       </div>
       {quote && (
-        <div className="space-y-3 md:py-3">
-          <h1 className="rounded-xl bg-zinc-700 px-6 py-3 text-center font-medium text-white md:text-2xl dark:bg-white dark:text-zinc-700">
-            "{quote.content}"
-          </h1>
+        <div className="">
+          <h1 className="">"{quote.content}"</h1>
           <div>
-            <p className="text-xs font-medium md:text-sm">Author :</p>
-            <p className="text-[8px] md:text-xs">{quote.author}</p>
+            <p className="">Author :</p>
+            <p className="">{quote.author}</p>
           </div>
-          <div className="flex justify-end space-x-2">
-            <button className="relative rounded-full border p-1 duration-300 hover:scale-105 hover:border-zinc-700">
+          <div className="">
+            <button className="">
               <ShareIcon />
             </button>
-            <button className="rounded-full border p-1 duration-300 hover:scale-105 hover:border-zinc-700">
+            <button className="">
               <StarIcon />
             </button>
           </div>
