a053b71e4a55ced5120f876c3747eb6e8efe3d08
amine alaoui <omari1amineco@gmail.com>
2024-03-07 22:05:14 +0100

    feat: add code to home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,31 @@
-import MainContainer from "@/components/MainContainer";
+import { useState } from "react";
 
 export default function Home() {
+  const [quote, setQuote] = useState();
+
+  const apiUrl = "https://api.quotable.io/random";
+
+  const getQuote = async () => {
+    const response = await fetch(apiUrl);
+    const quoteData = await response.json();
+
+    setQuote(quoteData);
+  };
+
   return (
     <main className="body">
-      <MainContainer />
+      <div>
+        <div>
+          <button onClick={getQuote}>Get a New Quote</button>
+        </div>
+        {/* If the fetch is successful... show this container */}
+        {quote && (
+          <div>
+            <h1>"{quote.content}"</h1>
+            <p>{quote.author}</p>
+          </div>
+        )}
+      </div>
     </main>
   );
 }
